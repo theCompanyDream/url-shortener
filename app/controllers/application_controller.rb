8ec40862daf7f741f695ac00c9045d90907b6096
Timothy Brantley II <tbrantleyii@gmail.com>
2021-05-13 19:42:15 -0400

    fix: params for controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,19 @@ class ApplicationController < ActionController::API
 		render json: { 'meta' => 'hello world'}
 	end
 
-	def create_hash(url)
+	def create_hash
+		url = params[:url]
 		hash = Hash.new(url)
-		$redis.set(url)
+		$redis.set(hash)
 	end
 
-	def get_hash(hash)
+	def get_hash
+		hash = params[:hash]
+
+		if hash == nil
+			
+		end
+
 		url = $redis.get(hash)
 		redirect_to url
 	end
